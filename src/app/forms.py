f42349a9329c5eb6d227109ceed28084ea02d3b2
zubairwazir <zubairwazir777@gmail.com>
2022-07-02 12:43:34 +0500

    adding forms functions

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,5 +1,6 @@
 from app.models import User
 
+
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
